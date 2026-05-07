aeda46da93f692b195988302d2636e73bee7a961
raseward14 <raseward14@gmail.com>
2026-05-07 17:12:42 -0600

    added a reactive form to the login2 component

--- src/app/auth/login2/login2.component.ts
+++ src/app/auth/login2/login2.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms';
 
 @Component({
     selector: 'app-login2',
@@ -6,4 +7,13 @@ import { Component } from '@angular/core';
     templateUrl: './login2.component.html',
     styleUrl: './login2.component.css',
 })
-export class Login2Component { }
+export class Login2Component {
+    richardsForm = new FormGroup({
+        email: new FormControl(''),
+        password: new FormControl('')
+    });
+
+    onSubmit() {
+        this.richardsForm.value.email;
+    }
+}
