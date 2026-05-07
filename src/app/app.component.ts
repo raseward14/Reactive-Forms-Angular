88b0edd24d523156f2c836b51d5f39f696b942d2
raseward14 <raseward14@gmail.com>
2026-05-07 16:51:02 -0600

    added login2 for the reactive forms approach

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,15 @@
 import { Component } from '@angular/core';
 
 import { LoginComponent } from './auth/login/login.component';
+import { Login2Component } from "./auth/login2/login2.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
   templateUrl: './app.component.html',
-  imports: [LoginComponent],
+  imports: [
+    // LoginComponent, 
+    Login2Component
+  ],
 })
-export class AppComponent {}
+export class AppComponent { }
---
+++ src/app/auth/login2/login2.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'app-login2',
+    standalone: true,
+    templateUrl: './login2.component.html',
+    styleUrl: './login2.component.css',
+})
+export class Login2Component { }
