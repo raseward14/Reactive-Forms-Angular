04e5d2a2d78d3f1f45cd4ae6d9c62b01dfbc66a2
raseward14 <raseward14@gmail.com>
2026-05-12 14:50:39 -0600

    added a custom validator to check whether or not the password has a question mark - updated the template error message to say this if that validator returns an object and not null

--- src/app/auth/login2/login2.component.ts
+++ src/app/auth/login2/login2.component.ts
@@ -1,10 +1,18 @@
 import { Component } from '@angular/core';
 import {
+    AbstractControl,
     FormControl,
     FormGroup,
     ReactiveFormsModule,
     Validators
 } from '@angular/forms';
+
+function mustContainQuestionMark(control: AbstractControl) {
+    if (control.value.includes('?')) {
+        return null;
+    }
+    return { doesNotContainQuestionMark: true }
+}
 
 @Component({
     selector: 'app-login2',
@@ -19,8 +27,8 @@ export class Login2Component {
             validators: [Validators.email, Validators.required]
         }),
         password: new FormControl('', {
-            validators: [Validators.required, Validators.minLength(6)]
-        })
+            validators: [Validators.required, Validators.minLength(6), mustContainQuestionMark],
+        }),
     });
 
     get emailIsInvalid() {
